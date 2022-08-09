24dffe29a4a51c247d49a13c1fa05d9fe9d00333
Amand YAPI <yapiamand@gmail.com>
2022-08-09 23:16:37 +0000

    update

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -103,19 +103,3 @@
 
 })(jQuery);
 
-document.addEventListener("DOMContentLoaded", function(){
-	window.addEventListener('scroll', function() {
-		if (window.scrollY > 50) {
-		  document.getElementById('navbar').classList.add('fixed-top');
-		  document.getElementById('navbar').classList.add('navbar-scroll');
-		  // add padding top to show content behind navbar
-		  navbar_height = document.querySelector('.navbar').offsetHeight;
-		  document.body.style.paddingTop = navbar_height + 'px';
-		} else {
-		  document.getElementById('navbar').classList.remove('fixed-top');
-		  document.getElementById('navbar').classList.remove('navbar-scroll');
-		   // remove padding top from body
-		  document.body.style.paddingTop = '0';
-		} 
-	});
-  });
